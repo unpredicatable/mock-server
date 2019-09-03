ceb115e9e14058c25c74a122362599188e500c35
Xu Mingxing <xumx@shinemo.com>
2019-09-03 15:08:35 +0800

    bcrypt-add

--- app/controllers/user_controllers.js
+++ app/controllers/user_controllers.js
@@ -1,6 +1,5 @@
 const query = require('../../query')
-
-
+const passport = require('../utils/passport')
 const get = async (ctx, next) => {
     console.log('ctx',ctx)
     ctx.status = 200;
@@ -27,7 +26,6 @@ const login = async (ctx ,next) => {
     const sql = "select passWord from userInfo where userName='"+req.userName+"'";
     // 获取用户的 userId
     const result = await query.queryData(req,sql);
-    let match = false
     if (!result) {
         ctx.status = 200;
         ctx.body = {
@@ -37,11 +35,12 @@ const login = async (ctx ,next) => {
         }
         return;
     }
-    result.length && result.map((cur,index) => {
-        if(cur.passWord == req.passWord){
-            match = true
-        }
-    })
+    const match = await passport.validate(req.passWord , result[0].passWord)
+    // result.length && result.map((cur,index) => {
+    //     if(cur.passWord == req.passWord){
+    //         match = true
+    //     }
+    // })
     ctx.status = 200;
     if (match) {
         ctx.body = {
@@ -60,9 +59,9 @@ const login = async (ctx ,next) => {
 
 const register = async (ctx,next) => {
     const req = ctx.request.body
+    const hash = await passport.encrypt(req.passWord)
     let sql = "select * from userInfo where userName='"+req.userName+"'";
     let uid = 20190901+ new Date().getTime()
-    let id = 0
     const result = await  query.queryData(req,sql)
     if(result.length > 0) {
         ctx.status = 200;
@@ -73,8 +72,8 @@ const register = async (ctx,next) => {
         }
         return
     }else {
-       let sql = "insert into userInfo set uid='"+uid+"'"+",userName='"+req.userName +"'"+",passWord='"+req.passWord+"'"+",tel='"+req.tel+"'"+",email='"+req.email+"'"
-        let registResult = await query.queryData(req,sql)
+        const sql = "insert into userInfo set uid='"+uid+"'"+",userName='"+req.userName +"'"+",passWord='"+hash+"'"+",tel='"+req.tel+"'"+",email='"+req.email+"'"
+        const registResult = await query.queryData(req,sql)
         if(registResult.insertId){
             ctx.status = 200;
             ctx.body = {
---
+++ app/utils/passport.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+
+
+
+//密码加密
+const encrypt = async (password, saltTimes) => {
+    const hash = await bcrypt.hash(password,10)
+    return hash
+};
+
+//密码hash校验
+const validate = async (password,hash) => {
+    const match = await bcrypt.compare(password, hash);
+    return match
+}
+
+module.exports = {
+    encrypt,
+    validate
+}
+
